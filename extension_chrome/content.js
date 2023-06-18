883d4ce9c8cac6ae38e6ff465d4a9e1e5c407ba0
Dan Popa <hey@hello.com>
2023-06-18 18:27:54 +0200

    have list of subtitles on video dom being found

--- extension_chrome/content.js
+++ extension_chrome/content.js
@@ -1,5 +1,7 @@
 console.log("Started 'Voice Over AI Netflix' extension.");
 
+let subtitles = [];
+
 function waitForElm(selector) {
   return new Promise((resolve) => {
     if (document.querySelector(selector)) {
@@ -12,8 +14,7 @@ function waitForElm(selector) {
         observer.disconnect();
       }
     });
-
-    observer.observe(document.body, {
+    observer.observe(document, {
       childList: true,
       subtree: true,
     });
@@ -22,10 +23,12 @@ function waitForElm(selector) {
 
 waitForElm("video").then((video) => {
   console.log(video.currentTime);
+  console.log({ subtitles });
 });
+
 /// end part 1
 
-var s = document.createElement("script");
+const s = document.createElement("script");
 s.src = chrome.runtime.getURL("inject.js");
 s.onload = function () {
   this.remove();
@@ -33,7 +36,26 @@ s.onload = function () {
 (document.head || document.documentElement).appendChild(s);
 
 document.addEventListener("yourCustomEvent", function (e) {
-  var data = e.detail;
-  console.log("content script");
-  console.log("received", e);
+  const data = e.detail;
+
+  const xmlStr = data;
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(xmlStr, "application/xml");
+  // print the name of the root element or error message
+  const errorNode = doc.querySelector("parsererror");
+  if (errorNode) {
+    console.log("error while parsing xml");
+  } else {
+    const listDom = Array.from(doc.querySelectorAll("p"));
+    subtitles = listDom.map((itemDom) => ({
+      id: itemDom.getAttribute("xml:id"),
+      begin: itemDom.getAttribute("begin"),
+      end: itemDom.getAttribute("end"),
+      text: itemDom.textContent,
+    }));
+    console.log("Subtitles parsed");
+  }
 });
+
+// 1057723332t
+// 106.890269
